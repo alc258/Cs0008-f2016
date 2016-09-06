a8970792085e43103c5ad7816a5bc606257bc695
alc258 <alc258@pitt.edu>
2016-09-06 13:02:34 -0400

    Executed exercise 3 from Chapter 1

---
+++ Ch1-ex3/Ch1-ex3.py
@@ -0,0 +1,16 @@
+#
+  # Template for code submission
+# Name       :Alyssa Chonko
+# Email      :alc258@pitt.edu
+# Date       :09/06.16
+# Class      :CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+
+# ...and now let's program with Python
