c1ca8c8d047b7382e50f4468ef90dbb5924cb4f0
alc258 <alc258@pitt.edu>
2016-09-27 21:11:08 -0400

    Executed exercise 7  from Chapter 2

---
+++ f2016_cs8_alc258_a1/f2016_cs8_alc258_a1.py
@@ -0,0 +1,19 @@
+# Template for code submission
+# Name       :Alyssa Chonko
+# Email      :alc258@pitt.edu
+# Date       :09/30/16
+# Class      :CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# Assignment 1
+# Notes:
+
+# ...and now let's program with Python
+
+#ask user if they are using USC
+unit=input("Are you using USC? ")
+distance=input("How far have you driven? ") #ask user to input the distance driven
+gas=input("How much gas have you used? ")   #ask user to input the amount of gas used
+if unit=="yes":
+    
